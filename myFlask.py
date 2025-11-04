eb535208053bbe605230c125e9e33556153544ce
DevCrishKha <devcrishkha@gmail.com>
2025-11-05 00:55:41 +0530

    Removed an anoying instruction for Gemini

--- myFlask.py
+++ myFlask.py
@@ -6,8 +6,8 @@ import markdown
 app = Flask(__name__)
 app.secret_key = "Devin_DevCrish"
 
-Session["SESSION_TYPE"] = "filesystem"
-Session["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = True
 Session(app)
 
 @app.route('/', methods=['POST', 'GET'])
@@ -17,7 +17,6 @@ def chat():
         system_instruction = (
             "You are a friendly, non-medical, and helpful health assistant. "
             "Your sole purpose is to provide general, educational health information and lifestyle tips. "
-            "You **must** start every response, regardless of the user's input, by reminding the user that you are not a doctor "
             "You **must** ask for extra details wherever required from the user to properly help them. "
             "and that they should consult a professional for medical advice. "
             "Do not engage in conversations outside of health, wellness, or general information."
